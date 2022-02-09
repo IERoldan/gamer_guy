3184abc02d81d49bb59a1a05f39bebe449a0c0f2
Ivan R <ivanroldan1989@gmail.com>
2022-02-09 10:35:05 -0300

    footer arreglado

--- js/loginValidation.js
+++ js/loginValidation.js
@@ -1,5 +1,57 @@
 const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 if(!currentUser) window.location.href = '/pages/login/login.html';
+const users = [
+    {
+    id: "7",
+    email: "boullhesendavid@gmail.com",
+    firstName: "David",
+    lastName: "Boullhesen",
+    password: "administrador",
+    role: "ADMIN_ROLE"
+    },
+    {
+    id: "8",
+    email: "ivanroldan1989@gmail.com",
+    firstName: "Ivan",
+    lastName: "Roldan",
+    password: "administrador",
+    role: "ADMIN_ROLE"
+    },
+    {
+    id: "9",
+    email: "dante.sarmientof5@gmail.com",
+    firstName: "Dante",
+    lastName: "Sarmiento",
+    password: "administrador",
+    role: "ADMIN_ROLE"
+    },
+    {
+    id: "10",
+    email: "lionelmessi@gmail.com",
+    firstName: "Lionel",
+    lastName: "Messi",
+    password: "alfabeta",
+    role: "CLIENT_ROLE"
+    },
+    {
+    id: "11",
+    email: "rodrigomartinez@gmail.com",
+    firstName: "Rodrigo",
+    lastName: "Martinez",
+    password: "alfabeta",
+    role: "CLIENT_ROLE"
+    },
+    {
+    id: "12",
+    email: "martinrodriguez@gmail.com",
+    firstName: "Martin",
+    lastName: "Rodriguez",
+    password: "alfabeta",
+    role: "CLIENT_ROLE"
+    }
+    ];
+    
+    localStorage.setItem('users', JSON.stringify(users));
 
 const navOption = [
     { viewValue: 'Administrar juegos', path: '/pages/administracion/Administración.html', role: 'ADMIN_ROLE'},
