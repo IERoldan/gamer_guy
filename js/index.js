96e711e6439fc87a95b98529347afd5d98ff894a
Ivan R <ivanroldan1989@gmail.com>
2022-02-13 12:08:08 -0300

    Navbar actuzalizados

--- js/index.js
+++ js/index.js
@@ -13,16 +13,16 @@ function printCardGames() {
             <div class="card m-2" >
                 <img src="${game.image}" class="card-img-top" alt="...">
                     <div class="card-body">
-                    <h5 class="card-title"><a href="">${game.name}</a></h5>
+                    <h5 class="card-title"><a href="/pages/error404/error404.html">${game.name}</a></h5>
                                   <p class="card-text">${game.description}</p>
                     </div>
                     <ul class="list-group list-group-flush">
                     <li class="list-group-item"><strong>Categorias:</strong> ${game.category}</li>
                     </ul>
                   <div class="card-body">
-                    <a href="" class="card-link"><button type="button" class="btn btn-primary btn-sm"><i
+                    <a href="/pages/error404/error404.html" class="card-link"><button type="button" class="btn btn-primary btn-sm"><i
                           class="fas fa-info-circle"></i></button></a>
-                    <a href="#" class="card-link"><button type="button" class="btn btn-success btn-sm"><i
+                    <a href="/pages/error404/error404.html" class="card-link"><button type="button" class="btn btn-success btn-sm"><i
                           class="fas fa-shopping-cart"></i></button></a>
                   </div>
             </div>
