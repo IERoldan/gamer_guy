7c2b847925e77042a8946ac87746abdb9e5baf46
dante-sarmiento <dante.sarmientof5@gmail.com>
2022-02-18 13:37:26 -0300

    funcionalidad registro

--- js/signup.js
+++ js/signup.js
@@ -7,12 +7,19 @@ let users = localStorage.getItem('users');
 // transformarlos a un objeto js
 users = JSON.parse(users);
 
+class User {
+    constructor(nombreCompleto, correo, contraseña, canAccess = true) {
+        this.fullName = nombreCompleto;
+        this.email = correo;
+        this.password = contraseña;
+        this.role = 'CLIENT_ROLE';
+    }
+}
+
 
 // evitar que se recargue la pagina
 function login (evt){
     evt.preventDefault();
-    //const email = loginForm.elements.email.value;
-    //const password = loginForm.elements.password.value;
     const { email, password } = loginForm.elements;
     console.log(email.value, password.value);
 
@@ -24,13 +31,50 @@ function login (evt){
     // contraseña que esta en el objeto coincide con la que el user ingreso
 // DOS CASOS POSIBLES:
 // -alguno de los datos no es correcto
-    if(!user || user.password !== password.value) return console.error( 'Error en los datos ingresados')
+    if(!user || user.password !== password.value) {
+        // document.getElementById('boton').click();
+        let myCustomHTMLElement = document.createElement('div')
+            myCustomHTMLElement.innerHTML = 'El usuario fue creado correctamente';
+            myCustomHTMLElement.classList.add('custom-div')
+            document.body.appendChild(myCustomHTMLElement);
 
+
+        setTimeout(()=> {
+            myCustomHTMLElement.parentNode.removeChild(myCustomHTMLElement)
+            console.log(myCustomHTMLElement);
+        }, 4000)
+        return;
+        // return console.error( 'Error en los datos ingresados')
+    }
     // -redireccionarlo a la pagina del home
     // -guardar los datos del usuario actual
     localStorage.setItem('currentUser', JSON.stringify(user))
     window.location.href = '/index.html';
 }
 
+const registerUser = (evt) => {
+    // evt.preventDefault();
+    // const registerForm = document.querySelector('#registerForm')
+    // console.log(registerForm.elements);
+    // const {fullName, email, password } = registerForm.elements;
+    // const user = new User(fullName.value, email.value, password.value);
+    // console.log('nuevo usuario', User)
+    
+    evt.preventDefault();
+    // Levanto los usuarios ya registrados
+    let localUsers = JSON.parse(localStorage.getItem('users'));
+
+    // Tomo los datos del formulario
+    const registerForm = document.querySelector('#registerForm');
+    // Inicializo las variables que necesito tomar del formulario
+    const {email, password, fullName } = registerForm.elements;
+    const user = new User(fullName.value, email.value, password.value);
+
+    if(localUsers.some(localUser => localUser.email === user.email)) return alert('El usuario ya existe')
+    localUsers.push(user);
+    localStorage.setItem('users', JSON.stringify(localUsers))
+    registerForm.reset();
+    registerForm.elements[0].focus()
+}
 
 
